cc6bdb6c0a4ae8b9b1ebf814fa12707f8deadeb4
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-10-15 06:52:07 +0100

    embbedding function

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { generateProducts } from "./openai";
+import { embedProducts, generateEmbedding, generateProducts } from "./openai";
+import { produtosSimilares, todosProdutos } from "./database";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,29 @@ app.post('/generate', async (req, res) => {
     console.error(e);
     res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+
+app.post("/cart", async (req, res) => {
+  const { message } = req.body;
+  const embedding = await generateEmbedding(message);
+  if (!embedding) {
+    res.status(500).json({ error: 'Embedding não gerada' });
+    return;
+  }
+  const produtos = produtosSimilares(embedding);
+  res.json(produtos.map(p => ({ nome: p.nome, similaridade: p.similaridade })));
+});
+
+app.post('/embeddings', async (req, res) => {
+  await embedProducts();
+  console.log(todosProdutos());
+  res.status(201).end();
+});
+
+app.post('/embedding', async (req, res) => {
+  const { input } = req.body;
+  await generateEmbedding(input);
+  res.status(201).end();
 });
 
 export default app;
